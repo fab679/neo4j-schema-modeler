621ce578be3385150e182b208b9f8158b2e82907
fab679 <kamaufabisch1175@gmail.com>
2025-12-02 17:17:21 +0300

    vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // When publishing to GitHub Pages under the repository URL
-  // e.g. https://<user>.github.io/neo4j-schema-modeler/
-  base: "/neo4j-schema-modeler/",
   plugins: [react(), tailwindcss()],
 });
